08cf4eff63a6deca2ebcd23a1526e22454443c43
wakoong <woosik.koong@gmail.com>
2017-04-03 17:13:05 +0900

    [1. 추가] Route 폴더 안에 index & user_info

--- routes/login/user_info.js
+++ routes/login/user_info.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var path = require('path')
+var bodyParser = require('body-parser') 
 
 
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({extended: true}))
+
+router.post('/', function(req,res){
+    res.send({'email': req.body.email, 'password': req.body.password});
+})
 
 module.exports = router;
