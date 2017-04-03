abcf9acb93d167e52554fc4e7f8d9be9d83f8525
donghyunkim <donghyunkim@donghyunkimui-MacBook-Pro.local>
2017-04-03 11:07:15 +0900

    [1.추가] 라우팅 처리 파일 추가
    app.js, router 폴더 추가

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by donghyunkim on 2017. 3. 28..
  */
+var express = require("express");
+var router = express.Router();
+
+router.get("/",function(req,res){
+    res.send("<h1>hello!!!!!!!</h1>");
+});
+
+module.exports = router;
