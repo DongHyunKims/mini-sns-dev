f11be9ae36cea2e22e1d540045e54aeab6fb41d0
wakoong <woosik.koong@gmail.com>
2017-04-06 21:43:36 +0900

    (1.추가) Passport & Session

--- routes/login/user_info.js
+++ routes/login/user_info.js
@@ -1,49 +1,86 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-// var mysql = require('mysql')
+//var mysql = require('mysql')
 var bodyParser = require('body-parser') 
+//var flash = require('connect-flash')
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
 var session = require('express-session')
-var flash = require('connect-flash')
+var dbConnection = require("../../model/dbConnection");
 
 
-//DATABASE SETTING
-//노출되지 않는 곳에 보관하는 방법 고려 
+// DATABASE SETTING
+// 노출되지 않는 곳에 보관하는 방법 고려 
 // var connection = mysql.createConnection({
 //     host : '192.168.56.101',
 //     user : 'instabook_login',
 //     password : 'instabook',
-//     database : 'test'
+//     database : 'instadb'
 // });
 
-// connection.connect()
+//connection.connect()
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extended: true}))
-router.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session)
-app.use(flash())
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session)
+//app.use(flash())
+
+
+
+router.get('/', function(req, res) {
+ console.log("asdfasdf");
+ res.render('login.ejs')
+//  var msg;
+//  var errMsg = req.flash('error')
+//  if (errMsg) msg = errMsg;
+//  res.render('login.ejs', {'message' : msg });
+})
+
+passport.serializeUser(function(user, done) {
+    console.log('passport session save: ', user)
+    done(null, user)
+});
+
+passport.deserializeUser(function(id, done) {
+    console.log('passport session get id: ', id)
+    done(null, id);
+}) 
 
 passport.use('local-join', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
+
     passReqToCallback: true
     }, function(req, email, password, done) {
-        console.log('local-join callback called');
+        var query = dbConnection.query('SELECT * FROM USER_TB WHERE USER_EAMIL=? AND USER_PASSWORD=?', [email, password], function(err, rows) {
+            if(err) return done(err);
+
+            if (rows.length) {
+                //세션에 담을 정보 
+                req.session.some = rows[0];
+                // console.log(req.session.some)
+                console.log('user exists')
+                console.log(rows[0]);
+                return done(null, {'USER_EAMIL': email, 'id': rows[0]._ID});
+            } else {
+                return done(null, false, {message : 'Sorry, your email or password was incorrect.'})
+            }
+        })
     }
 ));
 
 router.post('/', passport.authenticate('local-join', {
-    successRedirect: '/board',
+    successRedirect: '/main',
     failureRedirect: '/login',
     failureFlash: true })
+
 )
 
 
@@ -55,7 +92,7 @@ router.post('/', passport.authenticate('local-join', {
 //     var email = req.body.email;
 //     var password = req.body.password;
 //     var responseData = {};
-//     var query = connection.query('INSERT INTO node (email, password) VALUES ($1, $2);', [email, password], function(err, rows) {
+//     var query = connection.query('INSERT INTO instatable (email, password) VALUES (?, ?);', [email, password], function(err, rows) {
 //         if (err) throw err;
 // 		if(rows[0]) {
 // 			console.log(rows[0].name)
